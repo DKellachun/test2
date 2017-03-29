76a04ab865c1ff24e5d927795dd306e2ca68b758
user_14 <user_14@mac-14deimac.local>
2017-03-29 12:41:06 +0800

    test2 stortboad add programming

---
+++ test2/yayatrueLoveViewController.swift
@@ -0,0 +1,24 @@
+//
+//  yayatrueLoveViewController.swift
+//  test2
+//
+//  Created by user_14 on 2017/3/29.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+import UIKit
+
+class yayatrueLoveViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
